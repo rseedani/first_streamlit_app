cedf24a0ee2daa68682a4bc2d4fbcff155eefabf
rseedani <154378325+rseedani@users.noreply.github.com>
2023-12-19 21:39:17 -0500

    Update streamlit_app.py
    added a UI pick list

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,7 @@ import pandas
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(fruit_list)
 
+#adding a pick list
+streamlit.multiselect("Pick some fruits:". list(fruit_list.index))
 
 
